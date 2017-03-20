b8194aa62433ac1b5de440b77aa962f9976c344b
sectumsemper <Kaci@Aela-2.local>
2017-03-20 04:09:00 +0100

    github backup of arithmancy
    sticking my games up on github for Reasons

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadlevel(string name){
+		Debug.Log("lvl load req for: "+name);
+		Application.LoadLevel(name);
+	}
+	public void quitreq(){
+		Debug.Log("quit req");
+		Application.Quit ();
+		}
+		
+	}
+
+
+	
+
---
+++ Assets/arithmancer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class arithmancer : MonoBehaviour {
+	int max;
+	int min;
+	int guess; //var initialisation
+	
+	public int maxguessall=10;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame(); //starts game
+	}
+	
+	void StartGame(){
+		min=1;
+		max=1000;
+		guess=Random.Range(
+			min,
+			max);//random guess	
+		max=max+1;
+		text.text=guess.ToString();
+	}
+	
+	public Text text; 
+		
+	
+	
+	// Update is called once per frame
+	void Update () { //the delicious meaty meat of the game
+
+		}
+		
+	
+	public void higher(){
+		min=guess;
+		NextGuess();
+	}
+	public void lower(){
+		max=guess;
+		NextGuess();
+	}
+	
+	void NextGuess() {
+		guess=Random.Range(min, max);//guess random in new range
+		text.text=guess.ToString();
+		maxguessall=maxguessall-1;
+		if (maxguessall<=0){
+			Application.LoadLevel("Lose");
+		}
+	}
+}  
